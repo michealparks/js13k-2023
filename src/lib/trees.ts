3892d30b28aa40fc9686ad2739dd58bfea00502d
Micheal Parks <michealparks1989@gmail.com>
2023-08-29 02:06:39 -0400

    reduce tree poly count

--- src/lib/trees.ts
+++ src/lib/trees.ts
@@ -4,11 +4,11 @@ import { instancedMesh, meshStandardMat } from './util/three'
 export let trees = (scene: THREE.Scene) => {
   let count = 1000
   let scale = 0.3
-  let radius = 0.1
+  let radius = 0.15
   let height = 0.5
   let mesh = instancedMesh(
-    new THREE.ConeGeometry(radius, height),
-    meshStandardMat({ color: 'green' }),
+    new THREE.ConeGeometry(radius, height, 4, 1, true),
+    meshStandardMat({ color: '#388E3C' }),
     count)
 
   for (let i = 0; i < count; i++) {
